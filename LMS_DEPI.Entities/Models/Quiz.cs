a5d95246c3dff1434ccb868416e00e20eaa0751d
Seif-Ayman <sifeosten5055@gmail.com>
2024-09-29 21:50:04 +0300

    fix

--- LMS_DEPI.Entities/Models/Quiz.cs
+++ LMS_DEPI.Entities/Models/Quiz.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using LMS.Models;
 
 namespace LMS_DEPI.Entities.Models
-{   public class Quiz
+{
+    public class Quiz
     {
         [Key]
         public int Id { get; set; }
