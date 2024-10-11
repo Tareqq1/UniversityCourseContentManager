2beddb0f82a3375cc84d0ab6ee6c6fcb01a98582
Seif-Ayman <sifeosten5055@gmail.com>
2024-10-12 00:35:41 +0300

    j

---
+++ LMS_DEPI/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_DEPI.Controllers
+{
+    [Authorize(Roles = "User")]
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // This will render Views/Student/Index.cshtml
+        }
+
+        // Add other student-specific actions
+    }
+}
---
+++ LMS_DEPI/Controllers/TeacherController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_DEPI.Controllers
+{
+    [Authorize(Roles = "Teacher")]
+    public class TeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // This will render Views/Teacher/Index.cshtml
+        }
+
+        // Add other teacher-specific actions
+    }
+}
