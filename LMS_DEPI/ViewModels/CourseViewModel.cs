6a6292d8201ff579d42b5b42a7c3721f6e362e06
Seif-Ayman <sifeosten5055@gmail.com>
2024-10-14 21:16:04 +0300

    Added CourseViewModel, User Welcoming

---
+++ LMS_DEPI/ViewModels/CourseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS_DEPI.APP.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Start Date is required.")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "End Date is required.")]
+        public DateTime EndDate { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Credits must be a non-negative number.")]
+        public int Credits { get; set; }
+
+        public int TeacherId { get; set; }
+
+    }
+}
